bee32b919f32a2f7de27eef24f33c7b4c240fb2a
alacambra <alacambra@gmail.com>
2015-02-03 10:54:06 +0100

    basic packagae structure

---
+++ src/main/java/com/poolingpeople/deployer/boundary/DockerApi.java
@@ -0,0 +1,14 @@
+package com.poolingpeople.deployer.boundary;
+
+/**
+ * Created by alacambra on 03.02.15.
+ */
+public class DockerApi {
+
+    String endPoint = "";
+
+    public void buildContainer(){
+
+    }
+
+}
