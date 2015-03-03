8531797c2433cbb8292e4641fd168e9dc76b784a
alacambra <alacambra@gmail.com>
2015-03-03 16:24:03 +0100

    set name to the deployer container

--- scripts/deployment/build.sh
+++ scripts/deployment/build.sh
@@ -4,4 +4,4 @@ sudo docker build --tag=deployer /home/ubuntu/deployer/
 sudo docker kill deployer
 sudo docker rm deployer
 
-sudo docker run -d -p 8080:8080 deployer
+sudo docker run -d --name deployer -p 8080:8080 deployer
