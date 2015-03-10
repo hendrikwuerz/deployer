c30da47bb72534142300d49b9435f2a2de8751d1
alacambra <alacambra@gmail.com>
2015-03-10 11:01:45 +0100

    added port for wf console

--- scripts/deployment/build.sh
+++ scripts/deployment/build.sh
@@ -4,4 +4,4 @@ sudo docker build --tag=deployer /home/ubuntu/deployer/
 sudo docker kill deployer
 sudo docker rm deployer
 
-sudo docker run -d --name deployer -p 8080:8080 deployer
+sudo docker run -d --name deployer -p 8080:8080 -p 9990:9990 deployer
