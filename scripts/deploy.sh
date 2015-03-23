e5bafb3f4dc52fd2d4f9ce852f3314306d8f0298
alacambra <alacambra@gmail.com>
2015-03-23 12:12:01 +0100

    deploy script execute also build.sh remotely

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,3 +4,4 @@ mvn clean package
 cp target/deployer.war scripts/deployment/
 cd scripts/deployment/
 scp -r . ubuntu@deployer.poolingpeople.com:/home/ubuntu/deployer
+ssh ubuntu@deployer.poolingpeople.com "cd /home/ubuntu/deployer/ && ./build.sh"
