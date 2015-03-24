3c223d033532519026e6af3c6f44b383ff81e1d7
Hendrik Wuerz <hendrik.wuerz@ion2s.com>
2015-03-24 12:03:34 +0100

    enabled caching of war files

--- src/main/java/com/poolingpeople/deployer/application/boundary/VersionsApi.java
+++ src/main/java/com/poolingpeople/deployer/application/boundary/VersionsApi.java
@@ -121,18 +121,20 @@ public class VersionsApi {
 
 //        logger.fine("getWarForVersion: input stream read: " + String.valueOf(warFileIS.available()));
 
-        // cache result
-//        try {
-//            File cacheFile = getCacheFile(version, area);
-//            FileOutputStream stream = new FileOutputStream(cacheFile);
-//            IOUtils.copy(warFileIS, stream);
-//            stream.close();
-//            logger.fine("Save war for caching in " + cacheFile.getAbsolutePath());
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
+        byte[] data = getBytesFromStream(warFileIS);
 
-        return getBytesFromStream(warFileIS);
+        // cache result
+        try {
+            File cacheFile = getCacheFile(version, area);
+            FileOutputStream stream = new FileOutputStream(cacheFile);
+            stream.write(data);
+            stream.close();
+            logger.fine("Save war for caching in " + cacheFile.getAbsolutePath());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return data;
 
     }
 
@@ -164,6 +166,11 @@ public class VersionsApi {
      *          The file where the data is stored
      */
     private File getCacheFile(String version, String area) {
+        // check folder to exists
+        File folder = new File("cache");
+        if(!folder.exists()) {
+            folder.mkdirs();
+        }
         return new File("cache/cache-" + area + "-version-" + version + ".war");
     }
 
